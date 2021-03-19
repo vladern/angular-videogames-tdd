24376edae8bb6e54ecee56e8c52c0bc0fc2cf9d9
vladern <vladys.3234@gmail.com>
2021-03-19 12:58:00 +0100

    set app rotuting to videogames module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'videogames',
+  loadChildren: () => import('./videogames/videogames.module').then(m => m.VideogamesModule)
+},
+{
+  path: '',
+  redirectTo: 'videogames',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
