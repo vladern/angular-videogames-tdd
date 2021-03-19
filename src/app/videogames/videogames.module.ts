fe7dd4b341dec9d928c5b61c5bc4e622f2edc152
vladern <vladys.3234@gmail.com>
2021-03-19 19:44:42 +0100

    import core and shared modules on videogames module

--- src/app/videogames/videogames.module.ts
+++ src/app/videogames/videogames.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { VideogamesRoutingModule } from './videogames-routing.module';
-
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
 import * as fromComponents from './components/index';
 
 @NgModule({
   declarations: [fromComponents.components],
   imports: [
     CommonModule,
-    VideogamesRoutingModule
+    VideogamesRoutingModule,
+    CoreModule,
+    SharedModule
   ]
 })
 export class VideogamesModule { }
