c7a08e6cee01f5649d27610901b2a45eb65e296c
vladern <vladys.3234@gmail.com>
2021-03-21 14:05:13 +0100

    videogames service

--- src/app/core/services/videogames/videogames.service.spec.ts
+++ src/app/core/services/videogames/videogames.service.spec.ts
@@ -1,16 +1,89 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { VideogamesService } from './videogames.service';
+import Ajv from 'ajv';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Genre, Videogame } from '@shared/models/videogames';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 describe('VideogamesService', () => {
   let service: VideogamesService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VideogamesService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [VideogamesService],
+    });
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new VideogamesService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('baseURL should be `http://localhost:8000/videogame`', () => {
+    expect(service.baseURL).toEqual('http://localhost:8000/videogame');
+  });
+
+  it('getVideogames should return a list of videogames', () => {
+    const videogameList: Videogame[] = [
+      {
+        id: '1',
+        title: 'Hades',
+        genreId: '1',
+        price: 16.79,
+        releaseDate: '2020-09-17',
+        tags: ['Action roguelike', 'roguelike', 'Action', 'indie'],
+      },
+      {
+        id: '2',
+        title: 'Valheim',
+        genreId: '2',
+        price: 16.79,
+        releaseDate: '2021-02-02',
+        tags: ['Open World Survival Craft', 'Online Co-Op', 'Survival'],
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(
+      new Observable((observer) => {
+        observer.next(new HttpResponse({ body: videogameList }));
+      })
+    );
+
+    service.getVideogames().subscribe(({ body }) => {
+      expect(body?.length).toBe(2);
+      expect(body).toEqual(videogameList);
+    });
+  });
+
+  it('getGenres should return a list of genres', () => {
+    const genreList: Genre[] = [
+      {
+        id: '1',
+        name: 'RPG',
+      },
+      {
+        id: '2',
+        name: 'Adventure',
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(
+      new Observable((observer) => {
+        observer.next(new HttpResponse({ body: genreList }));
+      })
+    );
+
+    service.getGenres().subscribe(({ body }) => {
+      expect(body?.length).toBe(2);
+      expect(body).toEqual(genreList);
+    });
   });
 });
--- src/app/core/services/videogames/videogames.service.ts
+++ src/app/core/services/videogames/videogames.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env';
+import { Genre, Videogame } from '@shared/models/videogames';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideogamesService {
 
-  constructor() { }
+  public baseURL = environment.apiURL + '/videogame';
+
+  constructor(private _http: HttpClient) { }
+
+  getVideogames(): Observable<HttpResponse<Videogame[]>> {
+    return this._http.get<HttpResponse<Videogame[]>>(this.baseURL+'/list');
+  }
+
+  getGenres(): Observable<HttpResponse<Genre[]>> {
+    return this._http.get<HttpResponse<Genre[]>>(this.baseURL+'/genre/list');
+  }
 }
